f83736be684ce14da8f9cc285ae6a36967619d5c
OindrilaDas27 <oindrila.das2708@gmail.com>
2024-07-11 16:26:58 +0530

    refactor inspo content

---
+++ frontend/src/components/InspoLandingPage/InspoAssets.jsx
@@ -0,0 +1,30 @@
+import idea1 from '../../assets/idea1.png';
+import idea2 from '../../assets/idea2.png';
+import idea3 from '../../assets/idea3.png';
+import idea4 from '../../assets/idea4.png';
+import video1 from '../../assets/video1.mp4';
+import video2 from '../../assets/video2.mp4';
+import video3 from '../../assets/video3.mp4';
+import profile1 from '../../assets/profile1.jpg';
+import profile2 from '../../assets/profile2.jpg';
+import profile3 from '../../assets/profile3.jpg';
+import popular1 from '../../assets/foryou1.png';
+import popular2 from '../../assets/foryou2.png';
+
+export const images = {
+    idea1,
+    idea2,
+    idea3,
+    idea4,
+    profile1,
+    profile2,
+    profile3,
+    popular1,
+    popular2,
+};
+
+export const videos = {
+    video1,
+    video2,
+    video3,
+};
---
+++ frontend/src/components/InspoLandingPage/InspoData.js
@@ -0,0 +1,65 @@
+import { images, videos } from "./InspoAssets";
+
+export const videosData = [
+    {
+        id: 1,
+        video: videos.video1,
+        profile: images.profile1,
+    },
+    {
+        id: 2,
+        video: videos.video2,
+        profile: images.profile2,
+    },
+    {
+        id: 3,
+        video: videos.video3,
+        profile: images.profile3,
+    },
+];
+
+export const ideasData = [
+    {
+        id: 1,
+        image: images.idea1,
+        title: 'Clean Girl Aesthetic',
+    },
+    {
+        id: 2,
+        image: images.idea2,
+        title: 'Euphoria Makeup Ideas',
+    },
+    {
+        id: 3,
+        image: images.idea3,
+        title: 'Chikankari lahenga designs',
+    },
+    {
+        id: 4,
+        image: images.idea4,
+        title: 'Boho Balcony Decor',
+    },
+];
+
+export const popularData = [
+    {
+        id: 1,
+        image: images.popular1,
+        title: 'Summer Vacay Fits',
+    },
+    {
+        id: 2,
+        image: images.popular2,
+        title: '2024 Bridal Essentials',
+    },
+    {
+        id: 3,
+        image: images.idea3,
+        title: 'Chikankari lahenga designs',
+    },
+    {
+        id: 4,
+        image: images.idea4,
+        title: 'Boho Balcony Decor',
+    },
+];
